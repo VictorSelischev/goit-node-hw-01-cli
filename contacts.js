7eeb3196195e49f3108977bd2702f9ce939a00e8
Виктор Селищев <95778687+VictorSelischev@users.noreply.github.com>
2023-01-18 14:55:34 +0200

    add contacts.json

---
+++ contacts.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const contactsPath = path.resolve('./db/contacts.json');
+
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+}
