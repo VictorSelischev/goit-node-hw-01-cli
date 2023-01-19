da030313bb6e9debd87ccd0f27410d02a4114699
Виктор Селищев <selischev.victor.kharkov@gmail.com>
2023-01-19 10:42:21 +0200

    create invokeAction

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const argv = require("yargs").argv;
+
 const {
   listContacts,
   getContactById,
@@ -5,7 +7,46 @@ const {
   addContact,
 } = require('./contacts.js');
 
-// listContacts();
-// getContactById(7).then(console.log());
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.log(contacts);
+      break;
+
+    case "get":
+      const getContact = await getContactById(id);
+      console.log(getContact);
+      break;
+
+    case "add":
+      const contactsArr = await addContact(name, email, phone);
+      console.log(contactsArr);
+      break;
+
+    case "remove":
+      const newContactsArr = await removeContact(id);
+      console.log(newContactsArr);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: 7 });
+// invokeAction({action: "add", name: 'Victor', email: 'victor@gmail.com', phone: '(067) 895-37-44'});
+invokeAction({ action: "remove", id: 2 });
+
+
+// invokeAction(argv);
+
+
+// const array = async () => { await listContacts() };
+// console.log(array);
+// console.log(listContacts());
+// getContactById(99);
 // addContact('Victor', 'victor@gmail.com', '(067) 895-37-44' );
-removeContact(5);
+// removeContact(5);
